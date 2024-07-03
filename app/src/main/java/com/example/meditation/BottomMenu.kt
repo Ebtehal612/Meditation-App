f5a2cd9ee3036d58ce833319646b4013b126198c
Ebtehal Ahmed <tota.mash99@gmail.com>
2024-07-03 03:45:26 +0300

    Meditation

---
+++ app/src/main/java/com/example/meditation/BottomMenu.kt
@@ -0,0 +1,10 @@
+package com.example.meditation
+
+import android.icu.text.CaseMap.Title
+import android.service.quicksettings.Tile
+import androidx.annotation.DrawableRes
+
+data class BottomMenu1(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
